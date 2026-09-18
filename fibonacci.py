b26e638947b6c8b739d60bc06514f94a11aa50d7
alex001p-lab <alex.king001p@gmail.com>
2026-09-18 12:14:14 +0530

    fibonacci and struct program

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+a=0
+b=1
+n=10
+print("FIBONACCI SERIES")
+for i in range(n):
+    print(a)
+    c=a+b
+    a=b
+    b=c
---
+++ structure.py
@@ -0,0 +1,8 @@
+student={
+    "rollno":131,
+    "name":"harshit",
+    "marks":90
+}
+print(student["rollno"])
+print(student["name"])
+print(student["marks"])
